97ee5327bca8ea9eadb100a64b05aac89c15aab9
eiderpeña <80364500+eider1939@users.noreply.github.com>
2022-09-19 00:45:45 -0500

    Update pregunta_08.sql

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -42,4 +42,4 @@
 --  >>> Escriba su codigo a partir de este punto <<<
 --
 --USE data_science;
-SELECT strftime('%Y', c23) as [YEAR], AVG(c21)  from tbl2 GROUP BY [YEAR];
+SELECT YEAR(C23) as YEAR , AVG(c21)  from tbl2 GROUP BY YEAR(c23);
